f135702f0e6a184ac56d1bf4b93fd9e958db8c54
isabelaaug <isabela.augusta@hotmail.com>
2020-06-04 02:02:53 -0300

    Configurado banco de dados SQLite. Criado tabelas, items, imagens dos itens. Definido rotas e métodos.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,42 +1,11 @@
 import express from 'express';
+import routes from './routes';
+import path from 'path';
 
 const app = express();
 
-app.use(express.json())
-
-const users = [
-    'Isabela',
-    'Diego',
-    'Lucas',
-    'Larissa',
-    'João',
-    'Luiz'
-];
-
-app.get('/users', (request, response) => {
-    console.log('Listagem de Usuários')
-    const search = String(request.query.search)  // Nome do parâmetro é definido na requisição
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;  // if/else ternário
-
-    return response.json(filteredUsers)
-});
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id)  // Envia o nome do parâmetro na própria rota
-    const user = users[id]
-
-    return response.json(user)
-});    
-
-app.post('/users', (request, response) => {
-    const data = request.body
-
-    const user = {
-        nome: data.name,
-        email: data.email
-    }   
-    
-    return response.json(user)
-});
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);  // Porta do localhost
