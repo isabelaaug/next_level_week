266961d1497a9ad33a73d8acada5b23a82401a99
isabelaaug <isabela.augusta@hotmail.com>
2020-06-05 15:11:43 -0300

    Exemplos dos conceitos de React

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
+  const [counter, setCounter] = useState(0);  // [valor do estado, função para alterar valor]
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World!</h1>
-      </header>
+    <div>
+      <Header title={`Hello World - Contador: ${counter}`}/>
+      <h1>{counter}</h1>
+      <h1>{counter * 2}</h1>
+      <button type='button' onClick={handleButtonClick}>Aumentar</button>
     </div>
   );
 }
