d877fa1b6d7fe5e62afe6fa07f87a4b9119f8cec
isabelaaug <isabela.augusta@hotmail.com>
2020-06-09 00:57:34 -0300

    Incluido upload de imagem e validação de dados

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,57 +1,37 @@
 import express from 'express';
-import knex from './database/connection';
+import multer from 'multer';
+import multerConfig from './config/multer';
+import ItemsController from './controllers/ItemsController';
+import PointsController from './controllers/PointsController';
+import { celebrate, Joi } from 'celebrate';
+
 const routes = express.Router();
+const upload = multer(multerConfig);
 
-routes.get('/items', async (request, response) => {
-    const items = await knex('items').select('*');
-    const serializedItems = items.map(item => {
-        return {
-            id: item.id,
-            title: item.title,
-            image_url: `http://localhost:3333/uploads/${item.image}`    
-        }
-    })
+const itemsController = new ItemsController;
+const pointsController = new PointsController;
 
-    return response.json(serializedItems);
-});
+routes.get('/items', itemsController.index);
 
-routes.post('/points', async (request, response) => {
-    const {
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-        items
-    } = request.body;
-    
-    const trx = await knex.transaction();
-
-    const insertedIds = await trx('points').insert({
-        image: 'image-fake',
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf
-    });
-
-    const point_id = insertedIds[0];
-
-    const pointItems = items.map((item_id: number) => {
-        return {
-            item_id,
-            point_id,
-        }
-    })
-
-    await trx('point_items').insert(pointItems);
-
-    return response.json({ success: true })
-});
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+routes.post(
+    '/points', 
+    upload.single('image'), 
+    celebrate ({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.string().required(),
+            latitude: Joi.string().required(),
+            longitude: Joi.string().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required(),
+        })
+    }, {
+            abortEarly: false
+    }),
+    pointsController.create);
 
 export default routes;
